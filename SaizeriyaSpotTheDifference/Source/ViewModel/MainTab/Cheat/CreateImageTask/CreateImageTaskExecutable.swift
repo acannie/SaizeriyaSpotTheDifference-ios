b2e9a95765d35879fb841e4dda0368164e7d3932
acannie <acannie0530@gmail.com>
2025-11-20 01:21:05 +0900

    画像処理のプロトコルを実装

---
+++ SaizeriyaSpotTheDifference/Source/ViewModel/MainTab/Cheat/CreateImageTask/CreateImageTaskExecutable.swift
@@ -0,0 +1,70 @@
+//
+//  CreateImageTaskExecutable.swift
+//  SaizeriyaSpotTheDifference
+//
+//  Created by SASAOKA Akane on 2025/11/20.
+//
+
+import CoreImage
+
+protocol CreateImageTaskExecutable {
+    var headerText: String { get }
+    func createImage() async throws -> CheatResultScreenViewModel.ImageType
+    func ciImageToRGBA8(
+        _ ciImage: CIImage,
+        context: CIContext,
+        targetSize: CGSize?
+    ) -> (
+        data: [UInt8],
+        width: Int,
+        height: Int
+    )?
+}
+
+extension CreateImageTaskExecutable {
+    func ciImageToRGBA8(
+        _ ciImage: CIImage,
+        context: CIContext,
+        targetSize: CGSize? = nil
+    ) -> (
+        data: [UInt8],
+        width: Int,
+        height: Int
+    )? {
+        let image = if targetSize != nil {
+            ciImage.transformed(
+                by: CGAffineTransform(
+                    scaleX: targetSize!.width/ciImage.extent.width,
+                    y: targetSize!.height/ciImage.extent.height
+                )
+            )
+        } else {
+            ciImage
+        }
+        let width = Int(image.extent.width)
+        let height = Int(image.extent.height)
+        let rowBytes = width * 4
+        var pixelData = [UInt8](repeating: 0, count: rowBytes * height)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Big.rawValue
+
+        guard let cgImage = CIContext(options: nil).createCGImage(image, from: image.extent),
+              let context = CGContext(
+            data: &pixelData,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: rowBytes,
+            space: colorSpace,
+            bitmapInfo: bitmapInfo
+        ) else {
+            return nil
+        }
+
+        context.draw(
+            cgImage,
+            in: CGRect(x: 0, y: 0, width: width, height: height)
+        )
+        return (pixelData, width, height)
+    }
+}
