6fd6693756f82963b13ea94e9ac13397dd1822c7
acannie <acannie0530@gmail.com>
2025-11-29 19:55:35 +0900

    Rgbをフォルダに分けた

---
+++ SaizeriyaSpotTheDifference/Source/Utility/Rgb/Rgb.swift
@@ -0,0 +1,21 @@
+//
+//  Rgb.swift
+//  SaizeriyaSpotTheDifference
+//
+//  Created by SASAOKA Akane on 2025/11/25.
+//
+
+import Foundation
+
+struct Rgb {
+    let r: CGFloat
+    let g: CGFloat
+    let b: CGFloat
+
+    var hex: String {
+        let r255 = Int(self.r * 255)
+        let g255 = Int(self.g * 255)
+        let b255 = Int(self.b * 255)
+        return String(format: "#%02X%02X%02X", r255, g255, b255)
+    }
+}
