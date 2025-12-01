d046ea949c0c0a2038a8113e845f0d0525d1ffb7
acannie <acannie0530@gmail.com>
2025-12-02 00:40:19 +0900

    CIFilterを利用した差分抽出は削除

--- SaizeriyaSpotTheDifference/Source/ViewModel/MainTab/Cheat/CreateImageTask/Executable/DifferenceExtractionTask.swift
+++ SaizeriyaSpotTheDifference/Source/ViewModel/MainTab/Cheat/CreateImageTask/Executable/DifferenceExtractionTask.swift
@@ -19,14 +19,6 @@ struct DifferenceExtractionTask: CreateImageTaskExecutable {
             throw CreateImageTaskError.unexpectedError
         }
 
-        // 画像の形式変換
-        let context = CIContext()
-        let ciImageLeft = CIImage(cgImage: cgImageLeft)
-        let ciImageRight = CIImage(cgImage: cgImageRight)
-
-        // ポスタライズ処理
-        let diffImage1 = try await computeDifferenceTransparent(context: context, imageLeft: ciImageLeft, imageRight: ciImageRight)
-
         // 差分を抽出
         let differenceCoordinates: Set<ImageCoordinate> = try await getDifferenceCoordinates(left, right)
         let differencesOnLeftImage = try await previewLeft.extractPixels(at: differenceCoordinates)
@@ -73,49 +65,6 @@ private extension DifferenceExtractionTask {
             }
         }
         return differentCoordinates
-    }
-
-    func computeDifferenceTransparent(
-        context: CIContext,
-        imageLeft: CIImage,
-        imageRight: CIImage
-    ) async throws -> UIImage {
-        // --- ① 軽くぼかす ---
-        let blurRadius: CGFloat = 2.0
-        let blurLeft = imageLeft.applyingFilter("CIGaussianBlur", parameters: ["inputRadius": blurRadius])
-        let blurRight = imageRight.applyingFilter("CIGaussianBlur", parameters: ["inputRadius": blurRadius])
-
-        // 1. difference blend
-        let diffFilter = CIFilter.differenceBlendMode()
-        diffFilter.inputImage = blurLeft
-        diffFilter.backgroundImage = blurRight
-        guard var diffOutput = diffFilter.outputImage else {
-            throw CreateImageTaskError.unexpectedError
-        }
-
-        // 2. 黒(0,0,0) → 透明 に変換する
-        let colorMatrix = CIFilter.colorMatrix()
-        colorMatrix.inputImage = diffOutput
-
-        // RGB はそのまま
-        colorMatrix.rVector = CIVector(x: 1, y: 0, z: 0, w: 0)
-        colorMatrix.gVector = CIVector(x: 0, y: 1, z: 0, w: 0)
-        colorMatrix.bVector = CIVector(x: 0, y: 0, z: 1, w: 0)
-
-        // アルファ値 = RGB の最大値（差分が大きいほど不透明）
-        colorMatrix.aVector = CIVector(x: 1, y: 1, z: 1, w: 0)
-        guard let transparentOutput = colorMatrix.outputImage else {
-            throw CreateImageTaskError.unexpectedError
-        }
-
-        // 3. CGImage 化
-        guard let cgimg = context.createCGImage(
-            transparentOutput,
-            from: transparentOutput.extent
-        ) else {
-            throw CreateImageTaskError.unexpectedError
-        }
-        return UIImage(cgImage: cgimg)
     }
 }
 
